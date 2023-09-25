44b8105b2c861fd83943d53e378b999d9a3ecb87
barkerj26 <143556348+barkerj26@users.noreply.github.com>
2023-09-25 03:34:00 -0700

    Finish visuals hw (#1)
    * make view
    
    * add .idea to gitignore
    
    * Delete .idea directory
    
    * remove unused import
    
    * Delete .idea directory

--- app/src/main/java/com/example/stadiumcheckers/MainActivity.java
+++ app/src/main/java/com/example/stadiumcheckers/MainActivity.java
@@ -2,67 +2,22 @@ package com.example.stadiumcheckers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.RectF;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setContentView(new SampleView(this));
     }
 
-    private static class SampleView extends View {
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        GameView view = findViewById(R.id.gameView);
+        LinearLayout layout = findViewById(R.id.layout);
 
-        // CONSTRUCTOR
-        public SampleView(Context context) {
-            super(context);
-            setFocusable(true);
-
-        }
-
-        @SuppressLint("DrawAllocation")
-        @Override
-        protected void onDraw(Canvas canvas) {
-
-            canvas.drawColor(Color.WHITE);
-
-            //1
-            Paint paint = new Paint();
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setColor(Color.GRAY);
-            RectF oval1 = new RectF(1000, 400, 1550, 1000);
-
-            Paint p1 = new Paint();
-            p1.setColor(Color.BLACK);
-
-            canvas.drawText("Ring 1", 30, 50, p1);
-            canvas.drawOval(oval1, paint);
-
-
-            //2
-            paint.setStyle(Paint.Style.STROKE);
-            paint.setColor(Color.BLUE);
-            RectF oval2 = new RectF(900, 300, 1650, 1100);
-
-            Paint p2 = new Paint();
-            p2.setColor(Color.GREEN);
-
-            canvas.drawText("ring 3", 75, 75, p2);
-            canvas.drawOval(oval2, paint);
-            RectF oval3 = new RectF(800, 200, 1750, 1200);
-            canvas.drawOval(oval3,paint );
-
-            RectF oval4 = new RectF(700, 100, 1850, 1300);
-            canvas.drawOval(oval4,paint );
-        }
+        view.setScale(layout.getWidth(), layout.getHeight());
+        view.invalidate();
     }
 }
